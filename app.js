38e9c60782ef16ea4f595588b8552347d46cb953
Eric Sayer <me@ericsayer.com>
2024-10-16 22:39:36 -0600

    refactor app

--- app.js
+++ app.js
@@ -2,28 +2,28 @@ import { cpNewMod, getModuleNumber, rdDir, showMenu, removeSolved } from "./modu
 
 
 async function main() {
-	
 	const menuChoice = await showMenu();
-	let options
-	let modNum
+
 	switch (menuChoice) {
-		case "Copy new Module":
-			options = await rdDir();
-			modNum = await getModuleNumber(options);
-			await cpNewMod(modNum);
+		case "Copy new Module": {
+			const moduleOptions = await rdDir();
+			const { modNum } = await getModuleNumber(moduleOptions);
+			await cpNewMod({ modNum });
 			main();
 			break;
+		}
 		case "Copy solutions to Module":
 			break;
 		case "Setup new Repo":
 			break;
-		case "Remove Solved":
-			options = await rdDir();
-			modNum = await getModuleNumber(options);
-			const answer = await rdDir(modNum);
-			await removeSolved(answer, modNum);
+		case "Remove Solved": {
+			const moduleOptions = await rdDir();
+			const { modNum } = await getModuleNumber(moduleOptions);
+			const activityOptions = await rdDir({ modNum });
+			await removeSolved(activityOptions, { modNum });
 			main();
 			break;
+		}
 		case "Exit":
 			break;
 	}
